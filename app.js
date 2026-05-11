053bb53b4acf34e50dbf1abf82b6996639126773
Massinissa Ider <ider.massinissa@hotmail.com>
2026-05-11 21:35:10 +0200

    database: création des modèles MongoDB - closes #2

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const authRoutes = require("./routes/auth");
+const catwayRoutes = require("./routes/catways");
+const reservationRoutes = require("./routes/reservations");
+const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connexion à MongoDB réussie"))
+  .catch((err) => console.error("Erreur de connexion à MongoDB :", err));
+
+app.use("/", authRoutes);
+app.use("/catways", catwayRoutes);
+app.use("/catways", reservationRoutes);
+app.use("/users", userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Serveur démarré sur le port ${process.env.PORT}`);
+});
